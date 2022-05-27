362b1c56701c0a3f26799f704eae9358dbbb3008
Dennis Corral <dennis.corral@outlook.com>
2022-05-26 21:02:11 -0400

    better solution

--- js/characterMatch.js
+++ js/characterMatch.js
@@ -1,35 +1,26 @@
-// exports.isCharacterMatch = function(string1, string2) {
-function isCharacterMatch(string1, string2){
-    let str1Obj = {}
-    let str2Obj = {}
+exports.isCharacterMatch = function(string1, string2) {
 
-    for(i = 0; i < string1.length; i++){
-        if(str1Obj.hasOwnProperty(string1[i])){
-            str1Obj[string1[i]] += 1
-        }else {
-            str1Obj[string1[i]] = 1
+    string1 = string1.toLowerCase().split('')
+    string2 = string2.toLowerCase().split('')
+    array1 = string1.sort(function (a,b){
+        if (a > b)
+            return -1;
+        if (a < b)
+            return 1;
+        return 0;
+        })
+    array2 = string2.sort(function (a,b){
+        if (a > b)
+            return -1;
+        if (a < b)
+            return 1;
+        return 0;
+        })
+    for(let i = 0; i < array1.length; i++){
+        if(array1[i] != array2[i]){
+            return false
         }
     }
-    for(j = 0; j < string2.length; j++){
-        if(str2Obj.hasOwnProperty(string2[j])){
-            str2Obj[string2[j]] += 1
-        }else {
-            str2Obj[string2[j]] = 1
-        }
-    } 
-    str1Obj = Object.entries(str1Obj)
-    str2Obj = Object.entries(str2Obj)
-   
-    str2Obj = str2Obj.sort(function (a,b){return a[1]-b[1]})
-    str1Obj = str1Obj.sort(function (a,b){return a[1]-b[1]})
-    let stringCheck = 0
-    for(let k = 0; k < str1Obj.length; k++){
-        if(str1Obj[k][1] === str2Obj[k][1]){
-            stringCheck++
-        }
-    }
-     return (stringCheck === str1Obj.length && str2Obj.length) ? true : false
-    
+    return true
 };
 
-console.log(isCharacterMatch('ddennnis', 'eddisnnnn'))
